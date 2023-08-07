acab8c1e630b7d834886b50c3335af26f4b9b721
RonnieGuo <guorongfly@gmail.com>
2023-08-06 21:09:14 -0600

    Show specific details about properties located in Vancouver including their average rating.

--- 1_queries/average_rating.sql
+++ 1_queries/average_rating.sql
@@ -1,7 +1,8 @@
-SELECT p.id, p.title, p.cost_per_night, AVG(pr.rating) AS average_rating
-FROM properties p
-JOIN property_reviews pr ON p.id = pr.property_id
-WHERE p.city = 'Vancouver' AND pr.rating >= 4
-GROUP BY p.id, p.title, p.cost_per_night
-ORDER BY p.cost_per_night
+SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
+FROM properties
+LEFT JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING avg(property_reviews.rating) >= 4
+ORDER BY cost_per_night
 LIMIT 10;
