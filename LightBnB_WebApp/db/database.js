df3521375b79d3ba1cccbd6d4c4fc13b49fd5241
RonnieGuo <guorongfly@gmail.com>
2023-08-07 16:42:30 -0600

    connect to the database using node-postgres

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -1,3 +1,13 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb_db'
+});
+pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
+
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
